94cef0f6b801f2ad6e8e9e5f1488a45c514d637d
Darshan rijal <darshanrijal0@gmail.com>
2024-12-17 13:09:25 +0545

    create landing page with shadcn custom variant

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,36 @@
+import Image from "next/image";
+import logo from "@/assets/logo.png";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
-  return <main>Front page</main>;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center gap-6 bg-gray-100 px-5 py-12 text-center text-gray-900 md:flex-row md:text-start lg:gap-12">
+      <div className="max-w-prose space-y-3">
+        <Image
+          src={logo}
+          alt="logo"
+          height={150}
+          width={150}
+          className="mx-auto md:ms-0"
+        />
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Create a{" "}
+          <span className="inline-block bg-gradient-to-r from-green-600 to-green-400 bg-clip-text text-transparent">
+            Perfect resume
+          </span>{" "}
+          in minutes
+        </h1>
+
+        <p className="text-lg text-gray-500">
+          Our <span className="font-bold">AI Resume builder</span> helps you
+          design a professional resume even if you&apos;re not very smart
+        </p>
+
+        <Button asChild size="lg" variant="premium">
+          <Link href="/resumes">Get started</Link>
+        </Button>
+      </div>
+    </main>
+  );
 }
