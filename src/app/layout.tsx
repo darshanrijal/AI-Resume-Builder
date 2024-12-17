f116bef25d6e92fbad06f34a732d4ff7022b534f
Darshan rijal <darshanrijal0@gmail.com>
2024-12-17 18:25:02 +0545

    add toploader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "next-themes";
+import Toploader from "nextjs-toploader";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
@@ -28,6 +29,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
         <body className={inter.className}>
+          <Toploader showSpinner={false} color="#0f8536" height={3} />
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
